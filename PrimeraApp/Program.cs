3899bf3b166b900368326d8ffa2165ae2dcc82cd
studiosdopplermusic@gmail.com <studiosdopplermusic@gmail.com>
2023-01-21 15:57:05 -0500

    first C#

---
+++ PrimeraApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+
+//Este es un espacio donde podemos escribir nuestras clases, pueden haber dos clases llamadas igual en dons nameSpaces diferentes
+namespace PrimeraApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello Word");
+        }
+    }
+
+}
+
+
+namespace PrimeraAppSegunda
+{ 
+    class Data{
+          
+    }
+}
+
